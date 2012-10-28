7ab026e4895e6d1218f966554386435119984ba7
dingq2081 <dingq2081@163.com>
2012-10-28 17:06:47 +0800

    first

---
+++ src/guessNumber/GuessNumber.java
@@ -0,0 +1,18 @@
+package guessNumber;
+
+public class GuessNumber {
+
+    private String randomNumber;
+
+    public GuessNumber(String randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    public String validate(String userInput) {
+        if(userInput.equals(randomNumber)){
+            return "4a0b";
+        }
+        return "3a0b";
+    }
+
+}
